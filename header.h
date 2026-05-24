b07c3a32adc9bc540c74acabffd8a9d15921be36
user <user@pop-os.speedport.ip>
2026-05-24 21:13:55 +0200

    Moved to VSC

---
+++ header.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+using std::ostream;
+
+// A stream buffer that does nothing
+class NullBuffer : public std::streambuf {
+public:
+	int overflow(int c) override {
+		return c; // pretend everything was written successfully
+	}
+};
+
+// A stream that uses the null buffer
+class NullStream : public std::ostream {
+public:
+	NullStream() : std::ostream(&buffer) {}
+
+private:
+	NullBuffer buffer;
+};
+
+//Functions in header.cpp
+extern std::vector<bool> TextToAsciiB(const std::string& s);
+extern string BitsToAscii(const std::vector<bool>& bits);
+extern void ReadFileToArray(const std::string& filename, std::vector<bool>& array, ostream& out);
+extern bool WriteToImage(unsigned char* img, size_t imgSize, const vector<bool>& s, ostream& out, int& bitI, int& stringI);
+extern void WriteBitsToFile(const std::string& filename, const std::vector<bool>& bits);
+extern string ReadFilenameFromImageC(unsigned char* imgC, unsigned char* imgR, int& bitI, int& stringI, ostream& out);
+extern void ReadDataFromImageC(unsigned char* imgC, unsigned char* imgR, int size, int& bitI, int& stringI, vector<bool>& decoded, ostream& out);
+//This effectivly is CreateFilelist()
+extern vector<string> GetFilenamesFromFolder(string path);
+extern bool EncodeFolder(const string& foldername, ostream& out);
+extern bool EncodeImage(const string& filename, ostream& out);
+extern bool DecodeImage(const string& eFilename, ostream& out);
+extern bool DecodeFolder(const string& eFoldername, ostream& out);
+extern bool createFolder(const string& name);
+extern bool checkEx(const string& path);
+extern void InvalidInputMessage(const string& details = "[No Details]");
+extern void EncodeImageNI(const string& ifilename, const string& cfilename, ostream& out);
+extern void DecodeImageNI(const string& oFilename, const string& eFilename, ostream& out);
+extern void EncodeFolderNI(const string& ofoldername, const string& cfilename, ostream& out);
+extern void DecodeFolder(const string& oFoldername, const string& eFoldername, ostream& out);
+extern std::uintmax_t ReadbSizeFromFile(const std::string& filename);
+extern void CheckFilelists(const vector<string>& FileList1, const vector<string>& FileList2, ostream& out);
+extern void ReccomendActionFilelistMismatch(const vector<string>& FileList1ORIGINAL, const vector<string>& FileList2ORIGINAL, ostream& out);
---
+++ main.cpp
@@ -0,0 +1,113 @@
+#pragma warning(disable : 4996)
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdio>
+
+#include "header.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+//a lot of performace upgrades needed argv set on pause
+int main(int argc, char* argv[])
+{
+	string placeholder;
+	std::ostream* out = nullptr;
+	std::ofstream file("output.txt");
+	NullStream nullout;
+
+	stbi_write_png_compression_level = 9;
+
+	switch (argc)
+	{
+	case 1:
+	{
+		cout << "Verbose (V), Fileoutput(F) or None (N):";
+		cin >> placeholder;
+
+		if (placeholder == "V") {
+			out = &std::cout;
+			file.close();
+			remove("output.txt");
+		}
+		else if (placeholder == "F") {
+			file.open("output.txt");
+			out = &file;
+		}
+		else if (placeholder == "N") {
+			out = &nullout;
+			file.close();
+			remove("output.txt");
+		}
+		else {
+			InvalidInputMessage();
+			return 0;
+		}
+
+		cout << "\nEncode (E) or Decode (D):";
+		cin >> placeholder;
+		if (placeholder == "E")
+		{
+			cout << "Enter Imagename or Foldername:";
+			cin >> placeholder;
+			if (checkEx(placeholder))
+			{
+				if (placeholder[placeholder.length() - 4] != '.')
+					EncodeFolder(placeholder, *out);
+				else
+					EncodeImage(placeholder, *out);
+			}
+			else InvalidInputMessage("Your input is neiter a File nor a folder");
+		}
+		else if (placeholder == "D")
+		{
+			cout << "Enter Encoded Filename or Foldername:";
+			cin >> placeholder;
+			if (checkEx(placeholder))
+			{
+				if (placeholder[placeholder.length() - 4] != '.')
+				{
+					DecodeFolder(placeholder, *out);
+				}
+				else
+				{
+					DecodeImage(placeholder, *out);
+				}
+			}
+			else InvalidInputMessage();
+		}
+	}
+
+	break;
+	//arg ("console") version
+	/*default:
+		if (argv[1] == "E" || argv[1] == "e")
+		{
+			//second arg is file/foldername to encode to
+			if (argv[2][strlen(argv[2]) - 4] != '.')
+			{
+				//Folder
+			}
+			else
+			{
+				//File
+			}
+
+			return 0;
+		}
+		if (argv[1] == "D" || argv[1] == "d")
+		{
+			return 0;
+		}
+		InvalidInputMessage();
+	*/
+	}
+	
+	cout << "Finished";
+}
