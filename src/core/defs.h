982a292d1437a9f5bf3af96aa5a9d7c2d7917319
MrHunor <moldpolice@tutamail.com>
2026-07-14 20:20:06 +0200

    Reworked time in state.out

--- src/core/defs.h
+++ src/core/defs.h
@@ -10,23 +10,21 @@ class stateClass
 public:
     int verbose;
     bool deleteOverflow;
-    //LINK ../../docs/core.md:6
+    // LINK ../../docs/core.md:6
     void out(const std::string &output, int importance, std::source_location location = std::source_location::current())
     {
-        if(importance>verbose) return;
+        if (importance > verbose)
+            return;
         std::string message = "";
-
-        if ( verbose >= 4 ) message += std::format("{}->",location.function_name());
-        message+=output;
         if (verbose >= 4)
         {
             auto now = std::chrono::system_clock::now();
-            //This may look weird but this just pads the message to x characters so the timestamps are cleanly aligned
-            std::cout<< std::format("{:<150}    @{}",message,now);
+            message += std::format("@{} -> ", now);
         }
-        else std::cout<<message;
-
-        std::cout<<std::endl<<std::flush;
-    
+        if (verbose >= 4)message += std::format("{}->", location.function_name());
+        message += output;
+         std::cout << message;
+        std::cout << std::endl
+                  << std::flush;
     }
 };
